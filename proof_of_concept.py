0ba65751ac17f0aebc091a956227cd31682e84ee
Petr <szt.pracovni1@gmail.com>
2020-12-19 20:52:03 +0100

    Commented unused import

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-from appium import webdriver as appium_driver
+# from appium import webdriver as appium_driver
 
 from unittest import TestCase
 from unittest import main as unittest_main
