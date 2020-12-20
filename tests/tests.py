e9a52b8e7d90c3aacc29f7d35543bcd887fdae09
Petr <szt.pracovni1@gmail.com>
2020-12-20 19:54:27 +0100

    Added test validate if framework works as expected

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,23 @@
 from base.basetest import BaseTest
+from selenium.webdriver.common.by import By
+
+#######
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+#######
+
+# class Login(BaseTest):
+#
+#     def test_valid_login(self):
+#         self.app.login_screen.login("petr", "heslo")
+#         pass
 
 
-class Login(BaseTest):
+class SearchSeznam(BaseTest):
 
-    def test_valid_login(self):
-        self.app.login_screen.login("petr", "heslo")
-        pass
+    def test_simple_search(self):
+        self.app.go_to_url("https://seznam.cz")
+        search = self.app.app_driver.find_element(By.CSS_SELECTOR, 'div.search-form input[name=q].input')
+        search.send_keys("hele")
+        search.send_keys(Keys.ENTER)
+        sleep(3)
