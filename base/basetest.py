5b28bc0ca3586203065cc96a22a1afb4d87217ff
Petr <szt.pracovni1@gmail.com>
2020-12-20 19:53:18 +0100

    Added todo

--- base/basetest.py
+++ base/basetest.py
@@ -7,6 +7,11 @@ from configuration import configuration as cfg
 
 
 class BaseTest(_TestCase):
+    # TODO rewrite this so that logging works
+    #  currently unittest rewires stdout and logging isn't working afterwards
+    #  see https://stackoverflow.com/questions/7472863
+    #  consider the metaclass approach
+    #  together with writing custom set_up and tear_down and possibly runner
 
     def setUp(self) -> None:
         self.app = AppUi(Firefox())
