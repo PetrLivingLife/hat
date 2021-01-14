7bb2280342110a6ac603efa9dfc5082735a33e37
Petr Szturc <szt.pracovni1@gmail.com>
2021-01-14 19:52:20 +0000

    feat: updated _testing.py with selenium tests. Not working yet.

--- _testing.py
+++ _testing.py
@@ -3,6 +3,7 @@ import logging
 from os import path, getcwd
 
 from configuration import configuration
+from base.basetest import BaseTest
 
 
 configuration.setup_logging(
@@ -13,8 +14,37 @@ configuration.setup_logging(
 
 log = logging.getLogger(__name__)
 
-log.debug("logger debug msg here")
-log.info("logger info msg here")
-log.warning("logger warning msg here")
-log.error("logger error msg here")
-log.critical("logger critical msg here")
+# log.debug("logger debug msg here")
+# log.info("logger info msg here")
+# log.warning("logger warning msg here")
+# log.error("logger error msg here")
+# log.critical("logger critical msg here")
+
+################
+
+from selenium.webdriver.common.by import By
+
+#######
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+#######
+
+# class Login(BaseTest):
+#
+#     def test_valid_login(self):
+#         self.app.login_screen.login("petr", "heslo")
+#         pass
+
+
+class SearchSeznam(BaseTest):
+
+    def test_simple_search(self):
+        self.app.go_to_url("https://seznam.cz")
+        search = self.app.app_driver.find_element(By.CSS_SELECTOR, 'div.search-form input[name=q].input')
+        search.send_keys("hele")
+        search.send_keys(Keys.ENTER)
+        sleep(3)
+
+from unittest import main
+
+main()
