059ee8a4359885ca07178da454dcea357a1abc61
Petr Szturc <szt.pracovni1@gmail.com>
2021-01-29 18:37:18 +0000

    fix: clean up

--- _testing.py
+++ _testing.py
@@ -6,19 +6,7 @@ from configuration import configuration
 from base.basetest import BaseTest
 
 
-configuration.setup_logging(
-    # log_config_filepath="h",
-    # logs_output_path=path.join(getcwd(), "logs"),
-    # logging_level=logging.DEBUG,
-)
-
-log = logging.getLogger(__name__)
-
-# log.debug("logger debug msg here")
-# log.info("logger info msg here")
-# log.warning("logger warning msg here")
-# log.error("logger error msg here")
-# log.critical("logger critical msg here")
+configuration.setup_logging()
 
 ################
 
@@ -28,12 +16,6 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 #######
-
-# class Login(BaseTest):
-#
-#     def test_valid_login(self):
-#         self.app.login_screen.login("petr", "heslo")
-#         pass
 
 
 class SearchSeznam(BaseTest):
