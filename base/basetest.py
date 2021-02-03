aefbf300b11fe20be4f5902aa81086e2ae303bd5
Petr Szturc <szt.pracovni1@gmail.com>
2021-02-03 18:06:17 +0000

    fix: rewrote log object in setup and teardown method

--- base/basetest.py
+++ base/basetest.py
@@ -9,12 +9,11 @@ from configuration import configuration as cfg
 class BaseTest(object):
     
     def setup_method(self):
-        log = logging.getLogger(__name__)
-        log.info(f"Test case setup")
+        self.log = logging.getLogger(__name__)
+        self.log.info(f"Test case setup")
         self.app = AppUi(Firefox())
     
     def teardown_method(self):
-        log = logging.getLogger(__name__)
-        log.info(f"Cleaning after test.")
-        log.info(f"Closing application.")
+        self.log.info(f"Cleaning after test.")
+        self.log.info(f"Closing application.")
         self.app.quit()
