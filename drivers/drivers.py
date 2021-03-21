c77c46aa2279687162ad905c93f521d854161739
Petr Szturc <szt.pracovni1@gmail.com>
2021-03-21 18:55:46 +0000

    feature: united abstract into base driver
    * rewrote methods with @abstractmethod
    * replaced ._process_args() with *args, **kwargs
    * updated documentation

--- drivers/drivers.py
+++ drivers/drivers.py
@@ -12,34 +12,8 @@ from application import AppElement
 log = logging.getLogger(__name__)
 
 
-class AbstractDriver(ABC):
-    # TODO research and decide if to use AbstractClass
-    #   The main criteria is intuitivness especially for people from outside.
-    #   So that anyone writing their own driver easily understands what needs to be done, based on:
-    #       a log.error message with "Implement me __name__" or abstract class?
-    #   With abstract the BaseCustomDriver would be a bit cleaner.
-    #   It can also be implemented as another super class to inherit from;
-    #       with just the empty methods, not being abstract
-
-    @abstractmethod
-    def _start(self, driver_to_start,):
-        pass
-
-    @abstractmethod
-    def _open_app(self, *args, **kwargs,):
-        pass
-
-    @abstractmethod
-    def _get_element(self, locator,):
-        pass
-
-    @abstractmethod
-    def _close(self,):
-        pass
-
-
-class BaseCustomDriver(AbstractDriver):
-    """Basic implementation with all methods that should every driver support.
+class BaseCustomDriver(ABC):
+    """Basic/abstract implementation with all methods that should every driver support.
 
     When implementing custom driver, you should inherit from this class and implement needed methods.
     You should implement (overwrite) methods only with underscore, e.g. _start()
@@ -56,7 +30,7 @@ class BaseCustomDriver(AbstractDriver):
         String by which drivers are resolved to be used for tests.
     """
 
-    # TODO implement these also into abstract class if possible
+    # TODO implement these as abstract as well if possible
     name: str
     locator_type: str
 
@@ -66,41 +40,42 @@ class BaseCustomDriver(AbstractDriver):
         self.name = self.name
         self.platform_driver = None
 
-    def _process_args(self,):
-        # TODO rethink if this is needed.
-        """
-        Takes arguments, attributes, parameters from Hat and passes them to drivers.
-        """
-        return {}
-
-    def _remap_methods(self, *args, **kwargs):
-        """Remaps any methods so they are the same (name, arguments, return type) across drivers."""
-        # Warn if new driver doesn't have this method implemented.
-        log.warning(f"Implement me: {__name__}.")
-        raise NotImplementedError
-
-    def _start(self, *args, **kwargs):
-        # Warn if new driver doesn't have this method implemented.
-        log.warning(f"Implement me: {__name__}.")
-        raise NotImplementedError
-
-    def start(self, headless=False):
+    def start(self, headless=False, *args, **kwargs):
         log.info(f"Starting platform and driver: {self.locator_type, self.name}")
         # run method returned by _start() and pass arguments to it
-        self.platform_driver = self._start(headless=headless, **self._process_args())
+        self.platform_driver = self._start(headless=headless, *args, **kwargs)
         return self
 
-    def _open_app(self, **kwargs):
-        # Warn if new driver doesn't have this method implemented.
-        log.warning(f"Implement me: {__name__}.")
-        raise NotImplementedError
+    @abstractmethod
+    def _start(self,):
+        pass
 
     def open_app(self, url):
         return self._open_app(url)
 
+    @abstractmethod
+    def _open_app(self,):
+        pass
+
+    def close(self,):
+        log.info(f"Closing platform and driver.")
+        return self._close()
+
+    @abstractmethod
+    def _close(self,):
+        self.get_element()
+        pass
+
     def get_element(self, app_element):  # -> AppElement:
-        """It is preffered to call this method with AppElement type.\n
-        Though for easy use it supports calls with bare locators as well.
+        """
+        Basic method to work with an application elements.
+
+        Parameters:
+            app_element (AppElement, string): AppElement is preffered.
+                Though you can provide locators in whatever form your driver (implementation) supports.
+
+        Returns:
+            TODO AppElement: element to perform operations on.
         """
         log.debug(f"Looking for element: {app_element}")
         if isinstance(app_element, AppElement):
@@ -108,22 +83,26 @@ class BaseCustomDriver(AbstractDriver):
         # If called with just locator:
         return self._get_element(app_element)
 
-    def _get_element(self, locator):  # -> AppElement:
-        # Warn if new driver doesn't have this method implemented.
-        log.warning(f"Implement me: {__name__}.")
-        # implement here calling the native driver method for finding/selecting element
-        # e.g. for playwright you would write something like:
-        #   return self.tab.query_selector(locator)
-        # you may need to process the locator in some way, coming from self.get_element()
-        # you should also implement logic to always return (new instance) AppElement even if the element is not present
-        #  and set it with proper attributes like .is_present = False/True, .is_displayed = False/True ...
-        raise NotImplementedError
+    @abstractmethod
+    def _get_element(self,):
+        """Implement call to the platform driver method to find/obtain element
 
-    def close(self,):
-        log.info(f"Closing platform and driver.")
-        return self._close()
+        e.g.:
+        playwright:
+            return self.tab.query_selector(locator)
+        selenium:
+            return self.driver.find_element(*processed_locator)
 
-    def _close(self,):
-        # Warn if new driver doesn't have this method implemented.
-        log.warning(f"Implement me: {__name__}")
-        raise NotImplementedError
+        You may need to process the locator, coming from self.get_element()
+        You should also implement logic to always return (new instance of) AppElement even
+        if the element is not present and set it with proper attributes like
+            .is_present = False/True,
+            .is_displayed = False/True
+            ...
+        """
+        pass
+
+    @abstractmethod
+    def _remap_methods(self,):
+        # TODO Rethink if this can be done better
+        pass
